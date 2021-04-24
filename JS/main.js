f995e66939b4e2b8053e81f3287b0b67a03bfeee
Darius <darius.jasiulis@gmail.com>
2021-04-24 20:18:38 +0300

    some updates

---
+++ JS/main.js
@@ -0,0 +1,9 @@
+
+function openNav () {
+    document.getElementById("sideNav").style.display = "block";
+    document.getElementById("Overlay").style.display = "block";
+}
+function closeNav() {
+    document.getElementById("sideNav").style.display = "none";
+    document.getElementById("Overlay").style.display = "none";
+}
