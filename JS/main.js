420cb317736f059ec9e088fb673de7e1ab153d38
Darius <darius.jasiulis@gmail.com>
2021-04-25 10:13:37 +0300

    Nav JS

--- JS/main.js
+++ JS/main.js
@@ -1,9 +1,9 @@
 
 function openNav () {
-    document.getElementById("sideNav").style.display = "block";
-    document.getElementById("Overlay").style.display = "block";
+    document.getElementById('sideNav').style.display = "block";
+    document.getElementById('overlay').style.display = "block";
 }
 function closeNav() {
-    document.getElementById("sideNav").style.display = "none";
-    document.getElementById("Overlay").style.display = "none";
+    document.getElementById('sideNav').style.removeProperty('display');
+    document.getElementById('overlay').style.removeProperty('display');
 }
